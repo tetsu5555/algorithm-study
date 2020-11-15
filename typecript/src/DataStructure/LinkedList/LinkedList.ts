d3c026caefe1edbb4314741382b4e641460df017
tetsu5555 <tetete1118@gmail.com>
2020-11-15 16:32:02 +0900

    LinkedList

---
+++ typecript/src/DataStructure/LinkedList/LinkedList.ts
@@ -0,0 +1,45 @@
+// 連結リストの要素
+class LinkedListNode {
+  public value: any;
+  public next: any;
+  
+  constructor(value: any) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  public head: any;
+  
+  constructor() {
+    this.head = null
+  }
+
+  // 末尾の要素を追加する
+  push(value: any) {
+    const node = new LinkedListNode(value);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+    while(current.next) {
+      current = current.next
+    }
+
+    current.next = node;
+  }
+
+  unshift(value: any) {
+    const node = new LinkedListNode(value);
+    node.next = this.head;
+    this.head = node;
+  }
+}
+
+export {
+  LinkedList,
+}
